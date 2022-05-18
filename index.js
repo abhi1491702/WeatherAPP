c2ffef5e08e1b9b1efcad5faec72d4662ee1a308
ABHISHEK KANOUJIA <60141858+abhi1491702@users.noreply.github.com>
2022-05-18 16:50:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const weatherApi ={
+    key:"7899d7ac0293a9c06163f3ed1b60a146" ,
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather"
+}
+
+const searchInputBox=document.getElementById('input-box');
+// Event Listener Function on keypress
+searchInputBox.addEventListener('keypress',(event)=>{
+    if(event.keyCode==13)
+    {
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weather-body').style.display='block';
+    }
+
+})
+
+
+// Get weather Report
+function getWeatherReport(city){
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then(weather =>{
+        return weather.json();
+    }).then(showWeatherReport);
+}
+
+// Show weather report
+function showWeatherReport(weather){
+    console.log(weather);
+    
+    let city=document.getElementById('city');
+    city.innerText=`${weather.name},${weather.sys.country}`;
+
+    let temperature=document.getElementById('temp');
+    temperature.innerHTML=`${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp=document.getElementById('min-max');
+    minMaxTemp.innerHTML=`${Math.floor(weather.main.temp_min)}&deg;C (min)/${Math.ceil(weather.main.temp_max)}&deg;C (max)`
+
+    let weathertype=document.getElementById('weather');
+    weathertype.innerText=`${weather.weather[0].main}`;
+
+    let date=document.getElementById('date');
+    let todayDate=new Date();
+    date.innerText=dateManage(todayDate);
+
+    if( weathertype.textContent=='Clear')
+    {
+     document.body.style.backgroundImage="url('image/clear.jpg')";
+    }
+    else if( weathertype.textContent=='Clouds')
+    {
+     document.body.style.backgroundImage="url('image/cloud.jpg')";
+    }
+    else if( weathertype.textContent=='Rain')
+    {
+     document.body.style.backgroundImage="url('image/rain.jpg')";
+    }
+    else if( weathertype.textContent=='Snow')
+    {
+     document.body.style.backgroundImage="url('image/snow.jpg')";
+    }
+    else if( weathertype.textContent=='Thunderstrome')
+    {
+     document.body.style.backgroundImage="url('image/thunderstrome.jpg')";
+    }
+    else if( weathertype.textContent=='Haze')
+    {
+     document.body.style.backgroundImage="url('image/haze.jpg')";
+    }
+    else if( weathertype.textContent=='Dust')
+    {
+     document.body.style.backgroundImage="url('image/dust.jpg')";
+    }
+
+}
+
+// Date manage
+function dateManage(dateArg){
+    
+    let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","friday","Saturday"];
+
+    let months=["January","february","March","April","May","June","July","August","September","October","November","December"];
+
+    let year=dateArg.getFullYear();
+    let month=months[dateArg.getMonth()];
+    let date=dateArg.getDate();
+    let day=days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+
+}
+
